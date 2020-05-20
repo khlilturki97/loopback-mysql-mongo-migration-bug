0efbc1fecccfaa6fe3b18f6f34bdcb1dd7ce3720
khlilturki97@gmail.com <InsAtmpI2908>
2020-05-20 13:26:16 +0100

    comment migrate schema funct

--- src/application.ts
+++ src/application.ts
@@ -39,50 +39,50 @@ export class TodoListApplication extends BootMixin(
 
 
 
-  async migrateSchema(options?: SchemaMigrationOptions) {
-
-    // 1. Run migration scripts provided by connectors
-
-    // @ts-ignore
-    if (options.existingSchema === 'drop') {
-      // @ts-ignore
-      options.models = options?.models.reverse();
-    }
-    // @ts-ignore
-    const operation = options.existingSchema === 'drop' ? 'automigrate' : 'autoupdate';
-    // Instantiate all repositories to ensure models are registered & attached
-    // to their datasources
-    const repoBindings = this.findByTag('repository');
-    await Promise.all(repoBindings.map(b => this.get(b.key)));
-    // Look up all datasources and update/migrate schemas one by one
-    const dsBindings = this.findByTag('datasource');
-    for (const b of dsBindings) {
-      const ds = await this.get(b.key);
-      // console.log(ds);
-      // @ts-ignore
-      if (operation in ds && typeof ds[operation] === 'function') {
-        console.log('Migrating dataSource %s', b.key);
-        // @ts-ignore
-        if (!options?.models || options?.models.length === 0) {
-          // @ts-ignore
-          await ds[operation](options.models);
-        } else {
-          const models = [];
-          for (const model of options?.models) {
-            // @ts-ignore
-            if (Object.keys(ds.definitions).includes(model)) {
-              models.push(model);
-            }
-          }
-          // @ts-ignore
-          await ds[operation](models);
-        }
-      } else {
-        // debug('Skipping migration of dataSource %s', b.key);
-      }
-    }
-
-
-    // await super.migrateSchema(options);
-  }
+  // async migrateSchema(options?: SchemaMigrationOptions) {
+  //
+  //   // 1. Run migration scripts provided by connectors
+  //
+  //   // @ts-ignore
+  //   if (options.existingSchema === 'drop') {
+  //     // @ts-ignore
+  //     options.models = options?.models.reverse();
+  //   }
+  //   // @ts-ignore
+  //   const operation = options.existingSchema === 'drop' ? 'automigrate' : 'autoupdate';
+  //   // Instantiate all repositories to ensure models are registered & attached
+  //   // to their datasources
+  //   const repoBindings = this.findByTag('repository');
+  //   await Promise.all(repoBindings.map(b => this.get(b.key)));
+  //   // Look up all datasources and update/migrate schemas one by one
+  //   const dsBindings = this.findByTag('datasource');
+  //   for (const b of dsBindings) {
+  //     const ds = await this.get(b.key);
+  //     // console.log(ds);
+  //     // @ts-ignore
+  //     if (operation in ds && typeof ds[operation] === 'function') {
+  //       console.log('Migrating dataSource %s', b.key);
+  //       // @ts-ignore
+  //       if (!options?.models || options?.models.length === 0) {
+  //         // @ts-ignore
+  //         await ds[operation](options.models);
+  //       } else {
+  //         const models = [];
+  //         for (const model of options?.models) {
+  //           // @ts-ignore
+  //           if (Object.keys(ds.definitions).includes(model)) {
+  //             models.push(model);
+  //           }
+  //         }
+  //         // @ts-ignore
+  //         await ds[operation](models);
+  //       }
+  //     } else {
+  //       // debug('Skipping migration of dataSource %s', b.key);
+  //     }
+  //   }
+  //
+  //
+  //   // await super.migrateSchema(options);
+  // }
 }
